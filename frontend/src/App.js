5299da42c75328191b016dd4d3ce47defed73821
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-02 23:34:54 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,17 @@ import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
+import MainPage from "./Pages/MainPage"
+import ChatPage from "./Pages/ChatPage"
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/MainPage" element={<MainPage />} />
+        <Route path="/ChatPage" element={<ChatPage />} />
       </Routes>
     </Router>
   );
