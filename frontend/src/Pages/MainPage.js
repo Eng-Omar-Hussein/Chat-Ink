30023fd43944e9c18db94baa9d7f9a83957f7f27
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-02 23:18:11 +0300

    MainPage

---
+++ frontend/src/Pages/MainPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import ChatList from "../Components/ChatList/ChatList";
+import { useNavigate } from 'react-router-dom';
+
+function MainPage() {
+    const navigate = useNavigate();
+
+    const handleChatClick = (chat) => {
+        // Navigate to the ChatPage and pass the chat data
+        navigate('/ChatPage', { state: { selectedChat: chat } });
+    };
+
+    return (
+        <div className='Main'>
+            <Navbar />
+            <ChatList onChatClick={handleChatClick} />
+        </div>
+    );
+}
+
+export default MainPage;
