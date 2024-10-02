779e6b687181e8fd19863861aac8f95a5fc87dd0
salmahazem14 <150745682+salmahazem14@users.noreply.github.com>
2024-10-03 00:02:25 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login'
+import Notifications from './Pages/Notifications'
+import AccountSettings from './Pages/AccountSettings'
 import SignUp from './Pages/SignUp'
 import MainPage from "./Pages/MainPage"
 import ChatPage from "./Pages/ChatPage"
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/MainPage" element={<MainPage />} />
         <Route path="/ChatPage" element={<ChatPage />} />
+        <Route path="/accountSettings" element={<AccountSettings />} />
+        <Route path="/notifications" element={<Notifications />} />
       </Routes>
     </Router>
   );
