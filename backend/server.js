68f6b34e564d76a4863ab149b84d7d2341adadf9
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-10 00:09:56 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connectDB = require('./config/db');
 const chatRoutes = require('./routes/chatRoutes');
 const messageRoutes = require('./routes/messageRoutes');
+const notificationRoutes = require('./routes/notificationRoutes');
+
 
 connectDB();
 
@@ -10,6 +12,8 @@ app.use(express.json());
 
 app.use('/api/chats', chatRoutes);
 app.use('/api/messages', messageRoutes);
+app.use('/api/notifications', notificationRoutes);
+
 
 const PORT =  5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
