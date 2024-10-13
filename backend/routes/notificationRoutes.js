e0a9f2e2ceec909bcd7e071bbfc4ac88fb9af770
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-13 21:57:37 +0300

    Update notificationRoutes.js

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -8,5 +8,6 @@ router.post('/', notificationController.createNotification);
 router.get('/:id', notificationController.getUserNotificationById );
 router.patch('/:id/read', notificationController.markAsRead);
 router.delete('/:id', notificationController.deleteNotification);
+router.get('/messages/:userId', notificationController.getMessagesByUserId);
 
 module.exports = router;
