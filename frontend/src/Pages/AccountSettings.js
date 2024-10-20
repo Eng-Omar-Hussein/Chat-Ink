46ea225d54be0c5c662870b59ea2d2245a1e37c3
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-21 02:34:45 +0300

    Update AccountSettings.js

--- frontend/src/Pages/AccountSettings.js
+++ frontend/src/Pages/AccountSettings.js
@@ -1,25 +1,26 @@
 import React from "react";
 import FormPlus from "../Components/signUp/FormPlus";
-import Logo from "../Assets/Logo.png";
 import ProfilePictureUploader from "../Components/AccountSettings/ProfilePictureUploader";
 const text = { fontSize: "32", fontWeight: "700", color: "black" };
 
 function AccountSettings() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
+    <div className="mb-5">
+      <div className="container d-flex flex-column align-items-center">
+       
+        <div className="col-12 d-flex flex-column align-items-center">
             <h1 style={{ fontSize: "25px", marginTop: "40px" }}>
               Account Settings
             </h1>
-          </div>
-
-          <div className="col-7">
+          
+        </div>
+     
+       
+          <div className="col-12 d-flex flex-column align-items-center">
             <ProfilePictureUploader />
           </div>
         </div>
-      </div>
+     
 
       <div className="d-flex flex-column align-items-center">
         <FormPlus edit={true} />
