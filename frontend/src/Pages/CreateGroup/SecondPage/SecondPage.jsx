7f7f666053fe4e5e2449d32c5cf96658689246b4
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-21 02:32:09 +0300

    Update SecondPage.jsx

--- frontend/src/Pages/CreateGroup/SecondPage/SecondPage.jsx
+++ frontend/src/Pages/CreateGroup/SecondPage/SecondPage.jsx
@@ -1,29 +1,46 @@
-import { useEffect, useState } from "react";
-import SearchInput from "../../../Components/SearchInput/SeacrhInput";
+import {useState } from "react";
 import AddUser from "../../../Components/AddUser/AddUser";
 import styles from "./styles.module.css";
 import user from "../../../Assets/Ellipse 308.png";
+import SearchBar from "../../../Components/SearchBar/SearchBar";
 import { useNavigate } from "react-router-dom";
+
 const DUMMY_USERS = [
   { name: "Moaz", img: user },
   { name: "Omar", img: user },
   { name: "ibrahim", img: user },
+  { name: "Ahmed", img: user },
+  { name: "Ali", img: user },
+  { name: "Rahma", img: user },
+  { name: "Salma", img: user },
 ];
+
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate()
+  const filteredUsers = DUMMY_USERS.filter((user) =>
+    user.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container col-12">
-      <div className="d-flex justify-content-between mt-5 c col-12">
-        <h2>Add group participants</h2>
+      <div className="d-flex justify-content-between align-items-center mt-5 col-12">
+      <h2>Add group participants</h2>
         <div className="col-3">
-          <SearchInput className={"col-12"} />
+          <SearchBar
+            className={"col-12"}
+            searchTerm={searchTerm}
+            onSearchChange={setSearchTerm}
+          />
         </div>
       </div>
-      {DUMMY_USERS.map((user) => {
-        return <AddUser name={user.name} img={user.img}></AddUser>;
-      })}
-      <div className="d-flex ">
-        <button className={`${styles.btn} ms-auto`} onClick={()=> {navigate('/createGroupThirdPage')}}>Next</button>
+
+      {filteredUsers.map((user) => (
+        <AddUser key={user.name} name={user.name} img={user.img} />
+      ))}
+
+      <div className="d-flex mb-5">
+      <button className={`${styles.btn} ms-auto`} onClick={()=> {navigate('/createGroupThirdPage')}}>Next</button>
       </div>
     </div>
   );
