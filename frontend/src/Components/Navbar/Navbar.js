61a696d78fa73e53d3b1b625327210fbcd25a8c5
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-21 02:15:29 +0300

    Update Navbar.js

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -11,28 +11,28 @@ function Navbar() {
   return (
     <div className="Nav">
       <nav className="navbar">
-        <NavLink to="/MainPage" className="nav-link px-4 fw-bold link">
-          <div className="logo">
+        <NavLink to="/MainPage" className="nav-link ml-5 fw-bold link">
+          <div className="logo ">
             <img src={logo} alt="ChatLink Logo" className="logo-img" />
             <h1 className="app-name">ChatLink</h1>
           </div>
         </NavLink>
 
         <div className="nav-links" id="navLinks">
-          <NavLink to="/addFriends" className="nav-link px-4 fw-bold link">
+          <NavLink to="/addFriends" className="nav-link fw-bold link">
             <div className="nav-item">
               <img src={AddFriend} alt="Add Friend" className="icon" />
               <span>Add Friends</span>
             </div>
           </NavLink>
 
-          <NavLink to="/notifications" className="nav-link px-4 fw-bold link">
+          <NavLink to="/notifications" className="nav-link fw-bold link">
             <div className="nav-item">
               <img src={Notifications} alt="Notifications" className="icon" />
               <span>Notifications</span>
             </div>
           </NavLink>
-          <NavLink to="/createGroup" className="nav-link px-4 fw-bold link">
+          <NavLink to="/createGroup" className="nav-link fw-bold link">
             <div className="nav-item">
               <img
                 src={CreateGroup}
@@ -43,12 +43,12 @@ function Navbar() {
             </div>
           </NavLink>
 
-          <div className="logout px-4 fw-bold">
+          <div className="logout fw-bold">
             <img src={Logout} alt="log-out" className="icon" />
             <span>Logout</span>
           </div>
 
-          <NavLink to="/accountSettings" className="nav-link px-4 fw-bold link">
+          <NavLink to="/accountSettings" className="nav-link fw-bold link">
             <div className="Profile">
               <img
                 src={AccountPhoto}
@@ -60,23 +60,37 @@ function Navbar() {
         </div>
       </nav>
 
+
+
+
       <div className="bottom-nav">
+        <NavLink to="/addFriends" className="nav-link">
         <div className="nav-item">
           <img src={AddFriend} alt="Add Friend" className="icon" />
           <span>Add Friends</span>
         </div>
-        <div className="nav-item">
+        </NavLink>
+
+
+       <NavLink to="/notifications" className="nav-link">
+       <div className="nav-item" >
           <img src={Notifications} alt="Notifications" className="icon" />
           <span>Notifications</span>
         </div>
+       </NavLink>
+
+
+        <NavLink to="/createGroup" className="nav-link">
         <div className="nav-item">
           <img
             src={CreateGroup}
             alt="Create Group"
             className="create_group_icon"
           />
-          <span>Create Group</span>
+          <span className="l">Groups</span>
         </div>
+        
+        </NavLink>
       </div>
     </div>
   );
