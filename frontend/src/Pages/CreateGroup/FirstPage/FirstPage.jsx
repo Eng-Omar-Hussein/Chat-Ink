694612f9a8cb0b0fcc52500fa003a715d16a08e6
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-21 02:30:42 +0300

    Update FirstPage.jsx

--- frontend/src/Pages/CreateGroup/FirstPage/FirstPage.jsx
+++ frontend/src/Pages/CreateGroup/FirstPage/FirstPage.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import SearchInput from "../../../Components/SearchInput/SeacrhInput"; // Check the spelling of the import path if needed
-import user from "../../../Assets/Ellipse 308.png";
+// import user from "../../../Assets/Ellipse 308.png";
 import groupPic from '../../../Components/icons/group.png';
 import CardComponent from '../../../Components/Groups/CardComponent';
 import { useNavigate } from "react-router-dom";
@@ -28,20 +27,25 @@ export default function App() {
 
   return (
     <div id="groups-container">
-      <h2>Public Groups</h2>
+      <h2 className="m-4">Public Groups</h2>
 
-      <div className="container mt-4">
-        <div className="row">
-          {
-            randomGroups.map((group, index) => (
-              <CardComponent key={index} groupName={group.groupName} groupPic={group.groupPic} />
-            ))
-          }
+  <div className="container mt-4">
+  <div className="row">
+    {randomGroups.length > 0 ? (
+      randomGroups.map((group, index) => (
+        <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex justify-content-center " key={index}>
+          <CardComponent groupName={group.groupName} groupPic={group.groupPic} />
         </div>
-      </div>
+      ))
+    ) : (
+      <div className="col-12 text-center">No groups available.</div>
+    )}
+  </div>
+</div>
+
 
       <h2 
-        style={{ fontWeight: "bold", fontSize: "25px", marginTop: "30px", color: "#598E92", cursor: "pointer" }} 
+        style={{ fontWeight: "bold", fontSize: "25px", marginTop: "30px", color: "#598E92", cursor: "pointer", marginBottom: "65px" , marginLeft:"25px" }} 
         onClick={() => { navigate('/createGroupSecondPage') }}
       >
         Create your own group
