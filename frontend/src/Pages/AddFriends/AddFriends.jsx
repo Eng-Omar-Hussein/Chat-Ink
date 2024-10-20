0dbf1df32eff495c7423727b83757b2eaeca5e79
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-21 02:30:11 +0300

    Update AddFriends.jsx

--- frontend/src/Pages/AddFriends/AddFriends.jsx
+++ frontend/src/Pages/AddFriends/AddFriends.jsx
@@ -1,18 +1,35 @@
-import SearchInput from "../../Components/SearchInput/SeacrhInput";
+import React, { useState } from "react";
 import AddUser from "../../Components/AddUser/AddUser";
 import user from "../../Assets/Ellipse 308.png";
+import SearchBar from "../../Components/SearchBar/SearchBar";
+
+
+
 const DUMMY_USERS = [
   { name: "Moaz", img: user },
   { name: "Omar", img: user },
   { name: "ibrahim", img: user },
+  { name: "Ahmed", img: user },
+  { name: "Ali", img: user },
 ];
 export default function AddFrineds() {
+   const [searchTerm, setSearchTerm] = useState("");
+
+   const filteredUsers = DUMMY_USERS.filter((user) =>
+     user.name.toLowerCase().includes(searchTerm.toLowerCase())
+   );
+
   return (
     <div className="container">
-      <SearchInput className={"col-3 mt-5"}></SearchInput>
-      {DUMMY_USERS.map((user) => {
-        return <AddUser name={user.name} img={user.img}></AddUser>;
-      })}
+        <div className="d-flex justify-content-end mt-5">
+        <SearchBar
+          searchTerm={searchTerm}
+          onSearchChange={setSearchTerm}
+        />
+        </div>
+       {filteredUsers.map((user) => (
+        <AddUser key={user.name} name={user.name} img={user.img} />
+       ))}
     </div>
   );
 }
