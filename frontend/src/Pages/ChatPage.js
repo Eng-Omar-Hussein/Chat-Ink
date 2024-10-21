35c18409fb41803f3b6fa3bc5e48ccbff6ec6603
Omar Hussein <117474007+Eng-Omar-Hussein@users.noreply.github.com>
2024-10-21 22:09:44 +0300

    Add files via upload

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -3,14 +3,16 @@ import { useLocation, useNavigate } from "react-router-dom";
 import ChatWindow from "../Components/ChatWindow/ChatWindow";
 import ChatList from "../Components/ChatList/ChatList";
 import "./ChatPage.css";
+import { useSelector } from "react-redux";
 
 function ChatPage() {
+  const loggedInUser = useSelector((state) => state.chat.loggedInUser);
+  const loggedInUserId = loggedInUser ? loggedInUser._id : null;
   const location = useLocation();
   const navigate = useNavigate();
   const [selectedChat, setSelectedChat] = useState(
     location.state?.selectedChat || null
   ); // Set initial state from navigation state
-
   const handleBackClick = () => {
     setSelectedChat(null); // Reset selectedChat when going back
     navigate("/MainPage"); // Go back to the main page
@@ -29,9 +31,11 @@ function ChatPage() {
         <div className=" chat-window">
           {selectedChat && (
             <ChatWindow
-              name={selectedChat.name}
-              profilePicture={selectedChat.img}
+              name={selectedChat.participants[0]._id===loggedInUserId? selectedChat.participants[1].firstName+' '+selectedChat.participants[1].lastName:selectedChat.participants[0].firstName+' '+selectedChat.participants[0].lastName}
+              profilePicture={selectedChat.participants[0]._id===loggedInUserId? selectedChat.participants[1].profilePic:selectedChat.participants[0].profilePic}
               onBackClick={handleBackClick}
+              roomName={selectedChat._id}
+              chats= {selectedChat.messages}
             />
           )}
         </div>
