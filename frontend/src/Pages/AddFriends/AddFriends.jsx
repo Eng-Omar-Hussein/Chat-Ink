7e3e5c9f4030c5879223dcb8afdc5cd8fbc8a6a3
Rahma.Ali <126357261+1-3-Rahma@users.noreply.github.com>
2024-10-22 19:23:44 +0300

    Update AddFriends.jsx

--- frontend/src/Pages/AddFriends/AddFriends.jsx
+++ frontend/src/Pages/AddFriends/AddFriends.jsx
@@ -7,27 +7,33 @@ import { useEffect, useState } from "react";
 
 export default function AddFrineds() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [notFriends, setNotfriends] = useState(
+
+  const [notfriends,setNotFriends] = useState(
     useSelector((state) => state.friends.data)
   );
+   
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(updateUser());
   }, []);
+
+  const filtered = notfriends?.filter((user) => {
+    const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+    return fullName.includes(searchTerm.toLowerCase());
+  });
+
 
   return (
     <div className="container">
       <div className="d-flex justify-content-end mt-5">
         <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
       </div>
-      {notFriends?.map((user) => {
+      {filtered?.map((user) => {
         return (
           <AddUser
             name={user.firstName + " " + user.lastName}
             img={user.profilePic}
             id={user._id}
-            friends={notFriends}
-            setFriends={setNotfriends}
           ></AddUser>
         );
       })}
